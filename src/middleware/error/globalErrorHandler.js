a021e17a47215a58a33b33d28b18f04880afe305
Mostafa Ragab <m.hassan323@alexu.edu.eg>
2023-08-16 15:22:20 +0300

    dev mode stack

--- src/middleware/error/globalErrorHandler.js
+++ src/middleware/error/globalErrorHandler.js
@@ -1,11 +1,17 @@
 export const globalErrorHandler = () => {
   return (error, req, res, next) => {
     const status = error.statusCode || 500;
-
-    res.status(status).json({
-      status: "failed",
-      message: error.message,
-      error,
-    });
+    process.env.MODE == "dev"
+      ? res.status(status).json({
+          status: "failed",
+          message: error.message,
+          error,
+          stack: error.stack,
+        })
+      : res.status(status).json({
+          status: "failed",
+          message: error.message,
+          error,
+        });
   };
 };
