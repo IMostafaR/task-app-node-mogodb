e11c5d7ad70ea08576dfb18673b7b6f5c2f05b66
Mostafa Ragab <m.hassan323@alexu.edu.eg>
2023-08-17 03:18:27 +0300

    fix minor issue

--- src/middleware/validation/validation.js
+++ src/middleware/validation/validation.js
@@ -13,23 +13,14 @@ export const validation = (schema) => {
 
     httpHeaderFields.forEach((field) => {
       if (req.headers.hasOwnProperty(field)) {
-        switch (field) {
-          case "token":
-            const decoded = jwt.verify(
-              req.headers.token,
-              process.env.SECRET_KEY
-            );
-            requestData.loggedInUser = decoded.id;
-            requestData.token = req.headers.token;
-            break;
-
-          default:
-            break;
+        if (field == "token") {
+          const decoded = jwt.verify(req.headers.token, process.env.SECRET_KEY);
+          requestData.loggedInUser = decoded.id;
+          requestData.token = req.headers.token;
         }
       }
     });
 
-    console.log(requestData);
     const { error } = schema.validate(requestData, { abortEarly: false });
 
     error && next(new AppError(error.message, 400));
